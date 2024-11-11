3ba5f584ce0dcf3bbe1930019ab1cb5bfa073e15
shiva402 <120438174+shiva402@users.noreply.github.com>
2024-11-11 15:13:28 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    // Map of Roman numerals to their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    
+    // Loop through each character in the string
+    for (let i = 0; i < s.length; i++) {
+        // Get the value of the current and next Roman numeral
+        const currentVal = romanMap[s[i]];
+        const nextVal = romanMap[s[i + 1]];
+        
+        // If current value is less than the next value, subtract it; otherwise, add it
+        if (currentVal < nextVal) {
+            total -= currentVal;
+        } else {
+            total += currentVal;
+        }
+    }
+    
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
